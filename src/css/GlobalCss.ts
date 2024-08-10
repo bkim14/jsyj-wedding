87d01f1b47598da416781d03b04485f34324c0d4
c35p <qnddhr12@gmail.com>
2024-08-10 15:46:09 +0900

    add deploy workflow

--- src/css/GlobalCss.ts
+++ src/css/GlobalCss.ts
@@ -19,7 +19,7 @@ const GlobalCss = css`
   }
   @font-face {
     font-family: 'Blockletter';
-    src: url('./font/Blockletter/Adlery-Pro-trial.ttf') format('truetype');
+    src: url('/assets/font/Blockletter/Adlery-Pro-trial.ttf') format('truetype');
     font-style: normal;
   }
   @font-face {
